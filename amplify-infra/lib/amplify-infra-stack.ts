31fa71e85e401652e4e0f4334af6d5dd75a28a6a
Mario Scalas <mario.scalas@linksmt.it>
2021-07-12 15:43:29 +0200

    Add Hello REST API to the stack

--- amplify-infra/lib/amplify-infra-stack.ts
+++ amplify-infra/lib/amplify-infra-stack.ts
@@ -1,16 +1,44 @@
 import * as cdk from "@aws-cdk/core";
+import * as lambda from "@aws-cdk/aws-lambda";
+import * as apigw from "@aws-cdk/aws-apigateway";
 import * as amplify from "@aws-cdk/aws-amplify";
+import { LambdaIntegration } from "@aws-cdk/aws-apigateway";
+
+import * as path from "path";
 
 export class AmplifyInfraStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const myLambda = new lambda.Function(this, "Lambda", {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      handler: "handler.handler",
+      code: lambda.Code.fromAsset(path.resolve(__dirname, "lambda"))
+    });
+
+    const myApiGw = new apigw.RestApi(this, "hello-api", {
+      defaultCorsPreflightOptions: {
+        allowOrigins: apigw.Cors.ALL_ORIGINS,
+        allowMethods: apigw.Cors.ALL_METHODS,
+        allowHeaders: ["*"]
+      }
+    });
+
+    myApiGw.root
+      .resourceForPath("hello")
+      .addMethod("GET", new apigw.LambdaIntegration(myLambda));
+
 
     const amplifyApp = new amplify.App(this, "amplify-demo-app", {
       sourceCodeProvider: new amplify.GitHubSourceCodeProvider({
         owner: "scalasm",
         repository: "amplify-cdk-demo",
         oauthToken: cdk.SecretValue.secretsManager("github-token")
-      })
+      }),
+      environmentVariables: {
+        "ENDPOINT": myApiGw.url,
+        "REGION": this.region
+      }
     });
     
     amplifyApp.addBranch("main");
