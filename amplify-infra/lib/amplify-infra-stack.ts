5cf87579a6e37ca3bc85f241cd57f618621b0d9c
Mario Scalas <mario.scalas@linksmt.it>
2021-07-12 14:37:06 +0200

    Version CDK app

---
+++ amplify-infra/lib/amplify-infra-stack.ts
@@ -0,0 +1,18 @@
+import * as cdk from "@aws-cdk/core";
+import * as amplify from "@aws-cdk/aws-amplify";
+
+export class AmplifyInfraStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const amplifyApp = new amplify.App(this, "amplify-demo-app", {
+      sourceCodeProvider: new amplify.GitHubSourceCodeProvider({
+        owner: "scalasm",
+        repository: "amplify-cdk-demo",
+        oauthToken: cdk.SecretValue.secretsManager("github-token")
+      })
+    });
+
+    amplifyApp.addBranch("main");
+  }
+}
